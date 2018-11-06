36a0118613522c2ed004569fde7006016d97c716
tgray39 <44621008+tgray39@users.noreply.github.com>
2018-11-06 16:39:17 -0500

    Update GroupProject.py
    more code

--- GroupProject.py
+++ GroupProject.py
@@ -16,17 +16,25 @@ def checker(buttons):
     global clicked
 
     #If button text is empty, and click is true (player 1, or X), put an X in the button
-    if buttons["text"] == " " and click == True:
+    if buttons["text"] == " " and clicker == True:
         buttons["text"] == "X"
         click = False
 
     #If button text is empty, and click is false (player 2, or O), put an O in the button and change 
     #to player 1 turns (click = true)
-    if buttons["text"] == " " and click == False:
+    if buttons["text"] == " " and clicker == False:
         buttons["text"] == "O"
-        click = True
+        clicker = True
 
+        #Checks wins horiontally
     elif (button1["text"] == "X" and button2["text"] == "X" and button3["text"] == "X" or 
           button4["text"] == "X" and button5["text"] == "X" and button6["text"] == "X" or 
-          button7["text"] == "X" and button8["text"] == "X" and button8["text"]                                                                              )
+          button7["text"] == "X" and button8["text"] == "X" and button9["text"] == "X" 
+          
+          #Checks wins diagnally
+          button3["text"] == "X" and button5["text"] == "X" and button7["text"] == "X" or 
 
+                                                                                        )
+          
+    
+                                                                    
