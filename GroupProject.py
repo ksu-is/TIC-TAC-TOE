f6f80000005656c2d7c1fd5a0f3af2f60b74fb83
tgray39 <44621008+tgray39@users.noreply.github.com>
2018-11-08 16:40:24 -0500

    Update GroupProject.py
    New stuff. Doesnt quite work yet but getting close

--- GroupProject.py
+++ GroupProject.py
@@ -14,6 +14,7 @@ clicker = True
 
 def checker(buttons):
     global clicked
+    clicker = True
 
     #If button text is empty, and click is true (player 1, or X), put an X in the button
     if buttons["text"] == " " and clicker == True:
@@ -26,15 +27,66 @@ def checker(buttons):
         buttons["text"] == "O"
         clicker = True
 
-        #Checks wins horiontally
+        #Checks win condition for X horiontally
     elif (button1["text"] == "X" and button2["text"] == "X" and button3["text"] == "X" or 
           button4["text"] == "X" and button5["text"] == "X" and button6["text"] == "X" or 
-          button7["text"] == "X" and button8["text"] == "X" and button9["text"] == "X" 
+          button7["text"] == "X" and button8["text"] == "X" and button9["text"] == "X" or
           
-          #Checks wins diagnally
+          #Checks win condition for X diagnally
           button3["text"] == "X" and button5["text"] == "X" and button7["text"] == "X" or 
-
-                                                                                        )
+          button1["text"] == "X" and button5["text"] == "X" and button9["text"] == "X" or
           
+          #Checks win condition for X veritcally
+          button1["text"] == "X" and button4["text"] == "X" and button7["text"] == "X" or
+          button2["text"] == "X" and button5["text"] == "X" and button8["text"] == "X" or
+          button3["text"] == "X" and button6["text"] == "X" and button9["text"] == "X"):
+
+            tkinter.messagebox.showinfo("The Winner is X (player 1)!")
+
+    #Checks win condition for O horiontally
+    elif (button1["text"] == "O" and button2["text"] == "O" and button3["text"] == "O" or 
+          button4["text"] == "O" and button5["text"] == "O" and button6["text"] == "O" or 
+          button7["text"] == "O" and button8["text"] == "O" and button9["text"] == "O" or
+          
+          #Checks win condition for O diagnally
+          button3["text"] == "O" and button5["text"] == "O" and button7["text"] == "O" or 
+          button1["text"] == "O" and button5["text"] == "O" and button9["text"] == "O" or
+          
+          #Checks win condition for O veritcally
+          button1["text"] == "O" and button4["text"] == "O" and button7["text"] == "O" or
+          button2["text"] == "O" and button5["text"] == "O" and button8["text"] == "O" or
+          button3["text"] == "O" and button6["text"] == "O" and button9["text"] == "O"):
+
+            tkinter.messagebox.showinfo("The Winner is O (player 2)!")
+                
     
-                                                                    
+buttons = StringVar()
+
+button1 = Button(gui, text = "", font= "Times 26 bold", height = 4, width = 8, command=lambda:checker(button1))                                                           
+button1.grid(row = 1, column = 0, sticky = S+N+E+W)
+
+button2 = Button(gui, text = "", font= "Times 26 bold", height = 4, width = 8, command=lambda:checker(button2))
+button2.grid(row = 1, column = 1, sticky = S+N+E+W)
+
+button3 = Button(gui, text = "", font= "Times 26 bold", height = 4, width = 8, command=lambda:checker(button3))
+button3.grid(row = 1, column = 2, sticky = S+N+E+W)
+
+button4 = Button(gui, text = "", font= "Times 26 bold", height = 4, width = 8, command=lambda:checker(button4))
+button4.grid(row = 2, column = 0, sticky = S+N+E+W)
+
+button5 = Button(gui, text = "", font= "Times 26 bold", height = 4, width = 8, command=lambda:checker(button5))
+button5.grid(row = 2, column = 1, sticky = S+N+E+W)
+
+button6 = Button(gui, text = "", font= "Times 26 bold", height = 4, width = 8, command=lambda:checker(button6))
+button6.grid(row = 2, column = 2, sticky = S+N+E+W)
+
+button7 = Button(gui, text = "", font= "Times 26 bold", height = 4, width = 8, command=lambda:checker(button7))
+button7.grid(row = 3, column = 0, sticky = S+N+E+W)
+
+button8 = Button(gui, text = "", font= "Times 26 bold", height = 4, width = 8, command=lambda:checker(button8))
+button4.grid(row = 3, column = 1, sticky = S+N+E+W)
+
+button9 = Button(gui, text = "", font= "Times 26 bold", height = 4, width = 8, command=lambda:checker(button9))
+button9.grid(row = 3, column = 2, sticky = S+N+E+W)
+
+gui.mainloop()
